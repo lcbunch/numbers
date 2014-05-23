b986f4716026f8f6727818780951ff725acecaf1
lcbunch <lbunch@vt.edu>
2014-05-23 14:20:18 -0600

    update

--- calculator.rb
+++ calculator.rb
@@ -3,12 +3,15 @@ firstvalue = gets.chomp.to_i
 puts "Enter your second value"
 secondvalue = gets.chomp.to_i
 puts "What do you want to add, subtract, multiply, or divide?"
-choice =
+choice =gets.chomp
 
 case choice
 when 'add'
-  def add(firstvalue, secondvalue)
-    return firstvalue + secondvalue
-  end
-  puts add(firstvalue, secondvalue)
+  puts "The answer is #{firstvalue + secondvalue}"
+when 'subtract'
+  puts "The answer is #{firstvalue - secondvalue}"
+when 'multiply'
+  puts "The answer is #{firstvalue * secondvalue}"
+when 'divide'
+  puts "The answer is #{firstvalue / secondvalue}"
 end
